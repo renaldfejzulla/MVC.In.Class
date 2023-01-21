6710575df3976c63efd4fa2479f53fb0a9810ff0
renald.fejzulla@gmail.com <renald.fejzulla@gmail.com>
2023-01-21 11:56:49 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVC.In.Class.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
